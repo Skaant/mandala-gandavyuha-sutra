3bddd4a9143470761986cd8f57c949d1705b61a2
rimarok <romaric.ruga@mailo.fr>
2022-09-16 08:37:54 +0200

    App: integrate CircleOfHumanoids

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import Humanoid from "./components/Humanoid";
 import Character from "./types/Character";
+import CircleOfHumanoids from "./components/CircleOfHumanoids";
 
 function App() {
   const buddha: Character = { name: "Le Seigneur Bouddha" };
@@ -10,6 +11,12 @@ function App() {
     <div className="App">
       <svg viewBox="0 0 300 100" xmlns="http://www.w3.org/2000/svg">
         <Humanoid x={50} y={50} character={buddha} />
+        <CircleOfHumanoids
+          x={50}
+          y={50}
+          r={30}
+          characters={[{ name: "a" }, { name: "b" }, { name: "c" }]}
+        />
       </svg>
     </div>
   );
