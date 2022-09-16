38b7a39381b284aa4c4127ff2d58fd99b57135bf
rimarok <romaric.ruga@mailo.fr>
2022-09-16 08:23:49 +0200

    App : create Character and display in Humanoid

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Humanoid from "./components/Humanoid";
+import Character from "./types/Character";
 
 function App() {
+  const buddha: Character = { name: "Le Seigneur Bouddha" };
   return (
     <div className="App">
       <svg viewBox="0 0 300 100" xmlns="http://www.w3.org/2000/svg">
-        <Humanoid x={50} y={50} />
+        <Humanoid x={50} y={50} character={buddha} />
       </svg>
     </div>
   );
