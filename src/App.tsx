5cca2a615387e6999431755b66eae93f3c7e366c
rimarok <romaric.ruga@mailo.fr>
2022-09-16 10:59:24 +0200

    App : change sizes + integrate Circle

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,25 @@ import "./App.css";
 import Humanoid from "./components/Humanoid";
 import Character from "./types/Character";
 import CircleOfHumanoids from "./components/CircleOfHumanoids";
+import Circle from "./components/Circle";
+import { COLORS } from "./enums/colors";
 
 function App() {
   const buddha: Character = { name: "Le Seigneur Bouddha" };
   return (
     <div className="App">
-      <svg viewBox="0 0 300 100" xmlns="http://www.w3.org/2000/svg">
-        <Humanoid x={50} y={50} character={buddha} />
+      <svg viewBox="0 0 500 500" xmlns="http://www.w3.org/2000/svg">
+        <Circle
+          x={250}
+          y={250}
+          r={200}
+          fill={COLORS.GREY}
+          place={{ name: "Sravasti" }}
+        />
+        <Humanoid x={250} y={250} character={buddha} />
         <CircleOfHumanoids
-          x={50}
-          y={50}
+          x={250}
+          y={250}
           r={30}
           characters={[{ name: "a" }, { name: "b" }, { name: "c" }]}
         />
