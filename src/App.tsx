fa57054f3aa8cae70ff63f4c89045b1470f1035a
rimarok <romaric.ruga@mailo.fr>
2022-09-16 11:21:08 +0200

    App : display places

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,21 @@ import Humanoid from "./components/Humanoid";
 import Character from "./types/Character";
 import CircleOfHumanoids from "./components/CircleOfHumanoids";
 import Circle from "./components/Circle";
-import { COLORS } from "./enums/colors";
+import PLACES from "./data/places";
 
 function App() {
   const buddha: Character = { name: "Le Seigneur Bouddha" };
   return (
     <div className="App">
       <svg viewBox="0 0 500 500" xmlns="http://www.w3.org/2000/svg">
-        <Circle
-          x={250}
-          y={250}
-          r={200}
-          fill={COLORS.GREY}
-          place={{ name: "Sravasti" }}
-        />
+        {[
+          PLACES.SRAVASTI,
+          PLACES.PARC_JETA,
+          PLACES.JARDIN_AUMONE,
+          PLACES.PAVILLON_PARE,
+        ].map((place) => (
+          <Circle x={250} y={250} place={place} />
+        ))}
         <Humanoid x={250} y={250} character={buddha} />
         <CircleOfHumanoids
           x={250}
