b157c1c239c21a6fd0e5e069353e751516a955ae
rimarok <romaric.ruga@mailo.fr>
2022-09-16 16:16:34 +0200

    Add data/characters : w/ 2 first lines of bosa

---
+++ src/data/characters.ts
@@ -0,0 +1,96 @@
+import Character from "../types/Character";
+
+const CHARACTERS: { [key: string]: Character } = {
+  SAGESSE_SUPREME_SAGESSE_PRIMORDIALE: {
+    name: "Doué de la Sagesse Suprême de la Sagesse Primordiale",
+  },
+  SAGESSE_SUPREME_ETRES: {
+    name: "Doué de la Sagesse Suprême des Êtres",
+  },
+  BANNIERE_FLAMMES_LUMINEUSES: {
+    name: "Bannière de Flammes Lumineuses",
+  },
+  BANNIERE_MONT_MEROU: {
+    name: "Bannière du Mont Mérou",
+  },
+  PUISSANCE_TERRE: {
+    name: "Puissance de la Terre",
+  },
+  PUISSANCE_JOYAUX: {
+    name: "Puissance des Joyaux",
+  },
+  TRESOR_TERRE: {
+    name: "Trésor de la Terre",
+  },
+  TRESOR_ESPACE: {
+    name: "Trésor de l'Espace",
+  },
+  BON_OEIL: {
+    name: "Bon oeil",
+  },
+  OEIL_PUR: {
+    name: "Oeil pur",
+  },
+  DIVINE_COURONNE: {
+    name: "Divine Couronne",
+  },
+  COURONNE_SAGESSE_ILLUMINE: {
+    name: "Couronne de Sagesse qui Illumine Toute la Dimension Absolue",
+  },
+  CHIGNON_ROI_BRAHMAS: {
+    name: "Chignon du Roi des Brahmas",
+  },
+  CHIGNON_ROI_NAGAS: {
+    name: "Chignon du Roi des Nagas",
+  },
+  GRAND_RAYON: {
+    name: "Grand Rayon de Lumière",
+  },
+  RAYON_IMMACULE: {
+    name: "Rayon Immaculé",
+  },
+  BANNIERE_MERITES: {
+    name: "Bannière des Mérites",
+  },
+  BANNIERE_SAGESSE: {
+    name: "Bannière de la Sagesse",
+  },
+  VOIX_BRAHMA: {
+    name: "Voix de Brahma",
+  },
+  VOIX_OCEAN: {
+    name: "Voix de l'Océan",
+  },
+  JAILLI_ENSEIGNEMENTS: {
+    name: "Jailli des Enseignements",
+  },
+  JAILLI_SUBLIME: {
+    name: "Jailli du Sublime",
+  },
+  SUBLIME_RAYONEMENT: {
+    name: "Sublime par le Rayonnement",
+  },
+  SUBLIME_VERTUS: {
+    name: "Sublime par les Vertus",
+  },
+  SOUVERAIN_ROI_TECTONAS: {
+    name: "Souverain Roi des Tectonas",
+  },
+  SOUVERAIN_ROI_ENSEIGNEMENTS: {
+    name: "Souverain Roi des Enseignements",
+  },
+  VOIX_SILENCE_PAIX: {
+    name: "Voix du Silence et de la Paix",
+  },
+  VOIX_IMBLOQUEE: {
+    name: "Voix Imbloquée",
+  },
+  EVEIL_MONT_LUMIERES: {
+    name: "Éveil d'un Mont Mérou de Lumières",
+  },
+  EVEIL_ESPACE: {
+    name: "Éveil de l'Espace",
+  },
+};
+
+export default CHARACTERS;
