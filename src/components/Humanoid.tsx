5b903e014faefb3b894388ea026035290c58a666
rimarok <romaric.ruga@mailo.fr>
2022-09-16 08:23:27 +0200

    components/Humanoid takes types/Character as prop

--- src/components/Humanoid.tsx
+++ src/components/Humanoid.tsx
@@ -1,19 +1,35 @@
 import React from "react";
+import Character from "../types/Character";
 
 type HumanoidProps = {
   x: number;
   y: number;
   size?: number;
+  character?: Character;
 };
 
-export default function Humanoid({ x, y, size = 20 }: HumanoidProps) {
+export default function Humanoid({
+  x,
+  y,
+  size = 20,
+  character,
+}: HumanoidProps) {
   const top = [x, y - size];
   const right = [x + size * 0.6, y];
   const bottom = [x, y + size];
   const left = [x - size * 0.6, y];
   return (
-    <polygon
-      points={[top, right, bottom, left].map(([x, y]) => `${x},${y}`).join(" ")}
-    />
+    <>
+      <polygon
+        points={[top, right, bottom, left]
+          .map(([x, y]) => `${x},${y}`)
+          .join(" ")}
+      />
+      {character && (
+        <text x={x} y={y - 30} textAnchor="middle" fontSize="5">
+          {character.name}
+        </text>
+      )}
+    </>
   );
 }
